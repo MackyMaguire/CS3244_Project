4e45090ea261f0e7508c5ca5ac15110ed90a32f7
Ray <50550351+ray-97@users.noreply.github.com>
2020-04-23 23:59:18 +0800

    Add files via upload
    Just some functions that can help with visualization and create plots that can be included in report.

---
+++ plot_model.py
@@ -0,0 +1,22 @@
+import keras
+from matplotlib import pyplot as plt
+
+# history refers to object returned from model.fit(...)
+
+def plot_accuracy(history):
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+def plot_loss(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
