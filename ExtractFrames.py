33c2c58d7ac919c3f37507efacf6c4689eebb52e
MackyMaguire <zhoutianyu68@gmail.com>
2020-04-23 16:59:25 +0800

    Modified ExtractFrames.py to fit folder structure

--- ExtractFrames.py
+++ ExtractFrames.py
@@ -1,100 +1,86 @@
 
-'''
-Folder Strucuture:
-/root
-    ExtractFrames.py
-    /data
-        /train
-            /frames
-                /Real
-                    /055_frames
-                        1.jpg
-                        ...
-                /Fake
-                    /033_097_frames
-                        1.jpg
-                        ...
-            /videos
-                /Real
-                    055.mp4
-                /Fake
-                    033_097.mp4
-        /test
-            /frames
-                /Real
-                    /055_frames
-                        1.jpg
-                        ...
-                /Fake
-                    /033_097_frames
-                        1.jpg
-                        ...
-            /videos
-                /Real
-                    055.mp4
-                /Fake
-                    033_097.mp4
-'''
-
-import os
-
 import face_recognition
+import random
+import os
 from os.path import join
+from shutil import copy
 from cv2 import VideoCapture, imread, imwrite, imshow, rectangle, cvtColor, COLOR_BGR2RGB
 
-BASE_VIDEO_FOLDER = 'data/train/videos'
-BASE_IMAGES_FOLDER = 'data/train/frames'
+def processVideo(src, realfake, counter):
+    #Split train:test 3:1
+    rand = random.random()
+    traintest = 'Train'
+    if rand > 0.75:
+        traintest = 'Test'
 
-def extractFrames(frames):
-    print('Extracting frames...')
-    for folder in ['Real', 'Fake']:
-        for video in os.listdir(join(BASE_VIDEO_FOLDER, folder)):
-            src = join(join(BASE_VIDEO_FOLDER, folder), video)
-            dst = join(join(BASE_IMAGES_FOLDER, folder), os.path.splitext(video)[0] + '_frames')
-            
-            os.mkdir(dst)
-            
-            reader = VideoCapture(src)
-            
-            frame_num = 1
-            
-            while reader.isOpened():
-                running, frame = reader.read()
-                if not running:
-                    break
-                if frame_num > frames:
-                    break
-                imwrite(join(dst, '%d.jpg' % frame_num), frame)
-                frame_num += 1
+    #Extract face frames from each video
+    #Store frames in an individual folder for each video
+    frames_dst = join(traintest, realfake, 'Frames', str(counter))
+    os.mkdir(frames_dst)
+    extractFrames(frames, src, frames_dst)
+    
+    #Rename each video based on counter, which is in ascending numerical order
+    #Copy to new destination
+    video_dst = join(traintest, realfake, 'Videos', '%d.mp4' % counter)
+    copy(src, video_dst)
 
-            reader.release()
-            extractFace(dst)
+def extractFrames(frames, src, dst):
+    reader = VideoCapture(src)
+            
+    frame_num = 1
 
-# Replace all images inside a path with images of the extracted face from the frame
-def extractFace(path):
-    print('Extracting faces from frames for %s...' % path)
-    for img in os.listdir(path):
-        pixels = imread(join(path, img))
-        loc = face_recognition.face_locations(pixels)
+    while reader.isOpened():
+        running, frame = reader.read()
+        if not running:
+            break
+        if frame_num > frames:
+            break
 
+        #Extract face, with 25 pixels margin
+        loc = face_recognition.face_locations(frame)
         for (top, right, bottom, left) in loc:
-            rectangle(pixels, (left - 25, top - 25), (right + 25, bottom + 25), (0, 0, 255), 2)
-            # rectangle(pixels, (left, top), (right, bottom), (0, 0, 255), 2)
+            rectangle(frame, (left - 25, top - 25), (right + 25, bottom + 25), (0, 0, 255), 2)
+        face = frame[top - 25 : bottom + 25, left - 25 : right + 25]
 
-        extracted_img = pixels[top - 25 : bottom + 25, left - 25 : right + 25]
-        imwrite(join(path, img), extracted_img)
+        imwrite(join(dst, '%d.jpg' % frame_num), face)
+        frame_num += 1
+        
+    reader.release()
 
-def renameVideos():
-    print('Renaming videos...')
-    for folder in [join(BASE_VIDEO_FOLDER, 'Real'), join(BASE_VIDEO_FOLDER,'Fake')]:
-        counter = 1
-        for video in os.listdir(folder):
-            src = join(folder, video)
-            dst = join(folder, '%d.mp4' % counter)
-            os.rename(src, dst)
-            counter += 1
+def main(): 
+    frames = int(input("Enter desired number of frames to extract: "))
+
+    #Refer to README.md for outputted folder structure
+    for x in ['Train', 'Test']:
+        os.mkdir(x)
+        for y in ['Real', 'Fake']:
+            os.mkdir('%s/%s' % (x, y))
+            for z in ['Frames', 'Videos']:
+                os.mkdir('%s/%s/%s' % (x, y, z))
+
+    REAL_PATHS = ['data/original_sequences/actors/c23/videos',
+                  'data/original_sequences/youtube/c23/videos']
+
+    FAKE_PATHS = ['data/manipulated_sequences/DeepFakeDetection/c23/videos',
+                  'data/manipulated_sequences/Deepfakes/c23/videos',
+                  'data/manipulated_sequences/Face2Face/c23/videos',
+                  'data/manipulated_sequences/FaceSwap/c23/videos',
+                  'data/manipulated_sequences/NeuralTextures/c23/videos',]
+
+    fake_counter = 1
+    real_counter = 1
+            
+    for path in REAL_PATHS:
+        for video in os.listdir(path):
+            src = join(path, video)
+            processVideo(src, 'Real', real_counter)
+            real_counter += 1
+        
+    for path in FAKE_PATHS: 
+        for video in os.listdir(path):
+            src = join(path, video)
+            processVideo(src, 'Fake', fake_counter)
+            fake_counter += 1
 
 if __name__ == "__main__":
-    frames = int(input("Enter desired number of frames to extract: "))
-    # renameVideos()
-    extractFrames(frames)
+    main()
