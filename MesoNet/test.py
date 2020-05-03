7985d79c9dcb3389f1b70156871e4664222a2fa8
raikonen <choozeyuan97@gmail.com>
2020-05-03 12:35:18 +0800

    move test.py into folder

--- MesoNet/test.py
+++ MesoNet/test.py
@@ -1,35 +1,41 @@
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 
+import statistics
+import numpy as np
+
 from models import *
 from preprocessing import *
 
+'''
 def test_generator(X, y, dataGenerator, batch_size):
   for x_batch, y_batch in dataGenerator.flow(X, y, batch_size):
     yield(x_batch, y_batch)
+'''
 
 def evaluate_model(model, x_test, y_test):
-    test_datagen = ImageDataGenerator()
-    test_datagen.fit(x_test)
+  num_videos = len(x_test) // 40
+  num_correct = 0
+  for i in range(num_videos):
+    video = x_test[i*40:(i+1)*40]
+    pred_array = model.predict(video)
+    classification_array = map(lambda x: int(round(x[0])), pred_array)
+    arr = np.array(list(classification_array))
+    video_mean = np.bincount(arr).argmax()
+    classification = 1 if video_mean >= 0.5 else 0
+    if classification == y_test[i*40]:
+      num_correct += 1
 
-    batch_size = 32
-
-    test_gen = test_generator(x_test, y_test, test_datagen, batch_size)
-
-    total_step =  len(x_test) // batch_size
-
-    result = model.evaluate_generator(generator = test_gen, steps = total_step)
-
-    with open("{}_result.txt".format(model.name), 'w') as f:
-        f.write('loss: ' + str(result[0]) + '\n' + 'accuracy: ' + str(result[1]) + '\n')
+  with open("{}_result.txt".format(model.name), 'w') as f:
+    f.write('correctly classified: {}/{}\n'.format(num_correct,num_videos) + 'accuracy: ' + str(num_correct/num_videos) + '\n')
 
 def main():
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    real_path = os.path.join(current_directory,"Train/Real/Frames")
-    fake_path = os.path.join(current_directory,"Train/Fake/Frames")
+    real_path = os.path.join(current_directory,"Test/Real/Frames")
+    fake_path = os.path.join(current_directory,"Test/Fake/Frames")
 
     # To change to num_sample = (number_of_real_vids, number_of_fake_vids)
-    x_test, y_test = load_data(real_path,fake_path, num_sample = (1,1))
+    x_test, y_test = load_data(real_path,fake_path, num_sample = (2,8))
     
     meso4 = Meso4()
     meso4.load(os.path.join(current_directory,'Meso4.h5'))
